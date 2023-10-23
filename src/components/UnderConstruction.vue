959954732adbceb69f7846fbbff74764d5a6e315
Andrew <andrewkok80@gmail.com>
2023-10-23 09:07:42 -0400

    initial commit

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="under-construction">
+    <h1 class="green">Under Construction</h1>
+    <h3>
+      This page is under construction. Visit my old website at <a href="https://andrew-kwok.github.io/" target="_blank" rel="noopener">andrew-kwok.github.io</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
