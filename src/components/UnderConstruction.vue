5022e9184fe2eb2baf1444c147531a5bbbea6fbe
Andrew <andrewkok80@gmail.com>
2023-10-23 14:12:49 -0400

    add 3s auto redirect

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+  import { ref, onMounted } from "vue";
 
+  const countdown = ref(3);
+  onMounted(() => {
+    setInterval(() => {
+      countdown.value--;
+      if (countdown.value <= 0) {
+        window.location.href = "https://andrew-kwok.github.io/";
+      }
+    }, 1000);
+  });
 </script>
 
 <template>
@@ -8,6 +18,8 @@
     <h3>
       This page is under construction. Visit my old website at <a href="https://andrew-kwok.github.io/" target="_blank" rel="noopener">andrew-kwok.github.io</a>.
     </h3>
+
+    <p>Redirecting in {{ countdown }} seconds...</p>
   </div>
 </template>
 
