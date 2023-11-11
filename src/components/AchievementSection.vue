7ad12f95fe01c27fefbb9a07898ae04e0edc5613
Andrew <andrewkok80@gmail.com>
2023-11-11 11:13:40 -0500

    checkpoint

--- src/components/AchievementSection.vue
+++ src/components/AchievementSection.vue
@@ -1,11 +1,70 @@
 <script setup lang="ts">
 
+const achievements = [
+  {
+    "id": 1,
+    "title": "First Achievement",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eu ultricies fermentum, nunc libero ultricies urna, eget ultricies velit nisl eu nunc. Sed vitae nisl eget nunc aliquam aliquet. Sed vitae nisl eget nunc aliquam aliquet.",
+    "color": "blue",
+    "icon": "mdi-trophy"
+  },
+  {
+    "id": 2,
+    "title": "Second Achievement",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eu ultricies fermentum, nunc libero ultricies urna, eget ultricies velit nisl eu nunc. Sed vitae nisl eget nunc aliquam aliquet. Sed vitae nisl eget nunc aliquam aliquet.",
+    "color": "red",
+    "icon": "mdi-trophy"
+  },
+  {
+    "id": 3,
+    "title": "Third Achievement",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eu ultricies fermentum, nunc libero ultricies urna, eget ultricies velit nisl eu nunc. Sed vitae nisl eget nunc aliquam aliquet. Sed vitae nisl eget nunc aliquam aliquet.",
+    "color": "yellow",
+    "icon": "mdi-trophy"
+  }
+]
+
+
 </script>
 
 <template>
-  <div id="achievement" class="h-screen flex justify-center items-center">
-    <p class="text-7xl"> achievement </p>
-  </div>
+  <v-timeline direction="horizontal">
+    <v-timeline-item>
+      <template v-slot:opposite>
+        Opposite content
+      </template>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+
+    <v-timeline-item>
+      <template v-slot:opposite>
+        Opposite content
+      </template>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+
+    <v-timeline-item>
+      <template v-slot:opposite>
+        Opposite content
+      </template>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import Navbar from '@/components/NavBar.vue'
+  import HomeSection from "@/components/HomeSection.vue";
+  import EducationSection from "@/components/EducationSection.vue";
+  import ExperienceSection from "@/components/ExperienceSection.vue";
+  import ProjectSection from "@/components/ProjectSection.vue";
+  import ContactSection from "@/components/ContactSection.vue";
+  import AchievementSection from "@/components/AchievementSection.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeSection />
+    <Navbar />
+    <EducationSection />
+    <ExperienceSection />
+    <AchievementSection />
+    <ProjectSection />
+    <ContactSection />
   </main>
 </template>
