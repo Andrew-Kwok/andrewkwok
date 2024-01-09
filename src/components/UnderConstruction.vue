b8d5c315930599c5565c1604b7e9f7056817500c
Andrew <andrewkok80@gmail.com>
2024-01-09 01:20:01 -0500

    add resume link in construction panel

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -6,8 +6,9 @@
   <div class="card w-96 bg-base-100 shadow-xl">
     <div class="card-body">
       <h2 class="card-title">Under Construction</h2>
-      <p>Mobile site is currently under construction. </p>
-      <p> Please switch to a larger screen or visit my old site at <a class="link link-info" href="https://andrew-kwok.github.io/">https://andrew-kwok.github.io/</a> </p>
+      <p>Mobile site is currently under construction. Please switch to a larger screen </p>
+      <p> ... or visit my old site at <a class="link link-info" href="https://andrew-kwok.github.io/">https://andrew-kwok.github.io/</a> </p>
+      <p> ... or check my <a href="https://drive.google.com/file/d/1Ey755z_0UmQICr5OfS-ovD8A3VhBnYY7/view?usp=sharing" class="font-bold underline link">résumé</a> instead </p>
     </div>
   </div>
 </template>
